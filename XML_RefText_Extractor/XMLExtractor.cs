a6697e24143e6a20c8489e860ed0383029e3569a
Jonatah <benattijs@gmail.com>
2020-02-02 14:58:22 +1300

    Implement XML Extractor for RefText.

---
+++ XML_RefText_Extractor/XMLExtractor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XML_RefText_Extractor
+{
+    public class XMLExtractor
+    {
+        public IEnumerable<XElement> ExtracAll(string xmlInput, string[] refCodeFilter)
+        {
+            XDocument document = XDocument.Parse(xmlInput);
+            
+            IEnumerable<XElement> filtered = from el in document.Element("InputDocument").Element("DeclarationList").Element("Declaration").Element("DeclarationHeader").Elements("Reference")
+                            where refCodeFilter.Contains(el.Attribute("RefCode").Value)
+                            select el.Element("RefText");
+
+            return filtered;
+        }
+    }
+}
